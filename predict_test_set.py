523078c6aba66f8dd59686f10122d9a436cd38d5
Ben Schneider <schnei19@myumanitoba.ca>
2024-11-18 14:05:22 +0000

    [wip] update training code

--- predict_test_set.py
+++ predict_test_set.py
@@ -23,12 +23,10 @@ class TextDataset(Dataset):
     def __getitem__(self, index):
         
         x = self.x[index]
-        y = self.y[index]
 
         item = self.tokenizer(x, return_tensors="pt", truncation=True, max_length=max_length, padding="max_length")
         item["input_ids"] =  item["input_ids"].squeeze(dim=0)
         item["attention_mask"] = item["attention_mask"].squeeze(dim=0)
-        item["labels"] = y     
         return item
 
     def __len__(self):
@@ -38,8 +36,8 @@ class TextDataset(Dataset):
 def preprocess_dataset(tokenizer, train_ratio: float = 0.8, dataset_name='train.csv'):
     print("RUNNING " + dataset_name)
     train = pd.read_csv(dataset_name)
-
-    train.dropna(inplace=True)  # remove the nans
+    train.fillna(" ",inplace=True)  # remove the nans
+    train['Issue_id'] = train['Issue_id'].astype(int)
     train['Prompt'] = (('What is the priority (from 0, highest priority, to 4, lowest priority) of the code bug given the '
                        'following description. | Component: ') + train['Component'] + " | " + 'Title: ' + train['Title']
                        + " | " + 'Status: ' + train['Status'] + " | " + 'Resolution: ' + train['Resolution'] + " | " +
@@ -47,65 +45,44 @@ def preprocess_dataset(tokenizer, train_ratio: float = 0.8, dataset_name='train.
     
     # TODO why is this cast to numpy()?
     x = train['Prompt'].to_numpy()
-    y = train['Priority'].to_numpy(dtype=np.int32)
-    classes = {}
-    for inp, lab in zip(x, y):
-        if lab not in classes.keys():
-            classes[lab] = [inp]
-        else:
-            classes[lab].append(inp)
+    # dummy labels
+    y = [np.array(1) for _ in range(len(train))]
+  
+    train_dataset = TextDataset(x, y,tokenizer)
 
-    x_train = []
-    y_train = []
-    x_val = []
-    y_val = []
-    for key in classes:
-        r_train = np.random.choice(classes[key], int(train_ratio * len(classes[key])), replace=False).tolist()
-
-        r_val = list(set(classes[key]) - set(r_train))
-        x_train += r_train
-        x_val += r_val
-        y_train += [key]*len(r_train)
-        y_val += [key]*len(r_val)
-
-    train_dataset = TextDataset(x_train, y_train,tokenizer)
-    val_dataset = TextDataset(x_val, y_val, tokenizer)
-
-    return train_dataset, val_dataset
+    return train_dataset, train
 
 def compute_metrics(output):
-    labels = output.label_ids
     logits = output.predictions[0]
     preds = np.argmax(logits, axis=-1)
 
-    accuracy = (preds==labels).mean()
-    f1 = f1_score(labels, preds, average='macro')
-
-    return {'val_acc': accuracy, "val_f1": f1}
+    return preds
 
 def main():
-    dataset_file = sys.argv[-1]
-    tokenizer = AutoTokenizer.from_pretrained("google-t5/t5-large", max_length=max_length, truncation=True)
-    model = T5ForSequenceClassification.from_pretrained("google-t5/t5-large", num_labels=5)
+    dataset_file = "./shortened_test.csv"
+    local_path = '/home/b3schnei/cs846_best_runs/cs846_output/checkpoint-6575'
     parser = HfArgumentParser(TrainingArguments)
-    training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[-2]))[0]
+    training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[-1]))[0]
 
-    for param in model.parameters():
-        param.requires_grad = True
+    tokenizer = AutoTokenizer.from_pretrained(local_path, max_length=max_length, truncation=True)
+    model = T5ForSequenceClassification.from_pretrained(local_path, num_labels=5)
 
-    for param in model.classification_head.parameters():
-        param.requires_grad = True
-
-    train, val = preprocess_dataset(tokenizer, dataset_name=dataset_file)
+    train, df  = preprocess_dataset(tokenizer, dataset_name=dataset_file, train_ratio=1)
+    
+    #TODO remove this
+    train = torch.utils.data.Subset(train, list(range(100)))
 
     trainer = Trainer(model=model,
                     args=training_args,
                     train_dataset=train,
-                    eval_dataset=val,
                     tokenizer=tokenizer,
                     compute_metrics=compute_metrics
                     )
-    trainer.train()
+    out = trainer.predict(train)
+    preds = compute_metrics(out)
+    
+
+    print("pred done")
 
 if __name__ == '__main__':
     main()
