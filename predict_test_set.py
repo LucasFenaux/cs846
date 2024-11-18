60b2dc64f874bdb843b0c9fd6d961417b965b229
Ben Schneider <schnei19@myumanitoba.ca>
2024-11-18 16:01:01 +0000

    update predict

--- predict_test_set.py
+++ predict_test_set.py
@@ -23,66 +23,91 @@ class TextDataset(Dataset):
     def __getitem__(self, index):
         
         x = self.x[index]
+        y = self.y[index]
 
         item = self.tokenizer(x, return_tensors="pt", truncation=True, max_length=max_length, padding="max_length")
         item["input_ids"] =  item["input_ids"].squeeze(dim=0)
         item["attention_mask"] = item["attention_mask"].squeeze(dim=0)
+        item["labels"] = y     
         return item
 
     def __len__(self):
         return len(self.x)
 
 
-def preprocess_dataset(tokenizer, train_ratio: float = 0.8, dataset_name='train.csv'):
+def preprocess_dataset(tokenizer, train_ratio: float = 0.8, dataset_name='./shortened_test.csv'):
     print("RUNNING " + dataset_name)
     train = pd.read_csv(dataset_name)
-    train.fillna(" ",inplace=True)  # remove the nans
-    train['Issue_id'] = train['Issue_id'].astype(int)
+
+    train.dropna(inplace=True)  # remove the nans
     train['Prompt'] = (('What is the priority (from 0, highest priority, to 4, lowest priority) of the code bug given the '
                        'following description. | Component: ') + train['Component'] + " | " + 'Title: ' + train['Title']
                        + " | " + 'Status: ' + train['Status'] + " | " + 'Resolution: ' + train['Resolution'] + " | " +
-                       'Description: ' + train['Description'])
+                       'Description: ' + train['Shortened Description'])
     
-    # TODO why is this cast to numpy()?
     x = train['Prompt'].to_numpy()
-    # dummy labels
-    y = [np.array(1) for _ in range(len(train))]
-  
-    train_dataset = TextDataset(x, y,tokenizer)
+    
+    
+    y = train['Priority'].to_numpy(dtype=np.int32)
+    classes = {}
+    for inp, lab in zip(x, y):
+        if lab not in classes.keys():
+            classes[lab] = [inp]
+        else:
+            classes[lab].append(inp)
 
-    return train_dataset, train
+    x_train = []
+    y_train = []
+    x_val = []
+    y_val = []
+    for key in classes:
+        r_train = np.random.choice(classes[key], int(train_ratio * len(classes[key])), replace=False).tolist()
+
+        r_val = list(set(classes[key]) - set(r_train))
+        x_train += r_train
+        x_val += r_val
+        y_train += [key]*len(r_train)
+        y_val += [key]*len(r_val)
+
+    train_dataset = TextDataset(x_train, y_train,tokenizer)
+    val_dataset = TextDataset(x_val, y_val, tokenizer)
+
+    return train_dataset, val_dataset
 
 def compute_metrics(output):
+    labels = output.label_ids
     logits = output.predictions[0]
     preds = np.argmax(logits, axis=-1)
 
-    return preds
+    accuracy = (preds==labels).mean()
+    f1 = f1_score(labels, preds, average='macro')
+
+    return {'val_acc': accuracy, "val_f1": f1}
 
 def main():
-    dataset_file = "./shortened_test.csv"
-    local_path = '/home/b3schnei/cs846_best_runs/cs846_output/checkpoint-6575'
+    dataset_file = sys.argv[-1]
+    model_path = "/xyz"
+    tokenizer = AutoTokenizer.from_pretrained(model_path, max_length=max_length, truncation=True)
+    model = T5ForSequenceClassification.from_pretrained(model_path, num_labels=5)
     parser = HfArgumentParser(TrainingArguments)
-    training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[-1]))[0]
+    training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[-2]))[0]
 
-    tokenizer = AutoTokenizer.from_pretrained(local_path, max_length=max_length, truncation=True)
-    model = T5ForSequenceClassification.from_pretrained(local_path, num_labels=5)
+    for param in model.parameters():
+        param.requires_grad = True
 
-    train, df  = preprocess_dataset(tokenizer, dataset_name=dataset_file, train_ratio=1)
-    
-    #TODO remove this
-    train = torch.utils.data.Subset(train, list(range(100)))
+    for param in model.classification_head.parameters():
+        param.requires_grad = True
+
+    train, val = preprocess_dataset(tokenizer, dataset_name=dataset_file)
 
     trainer = Trainer(model=model,
                     args=training_args,
-                    train_dataset=train,
+                    train_dataset=None,
+                    eval_dataset=None,
                     tokenizer=tokenizer,
                     compute_metrics=compute_metrics
                     )
-    out = trainer.predict(train)
-    preds = compute_metrics(out)
-    
-
-    print("pred done")
+    #trainer.train()
 
 if __name__ == '__main__':
     main()
--- shorten_desc.py
+++ shorten_desc.py
@@ -76,5 +76,65 @@ def main_vllm():
 
     new_train.to_csv(f'shortened_{mode}.csv', index=False)
 
+
+def main():
+    # Change model path here
+    model_name = "neuralmagic/Meta-Llama-3.1-70B-Instruct-quantized.w4a16"
+
+
+
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_name#, max_length=max_length, truncation=True
+    )
+    # Set the padding token
+    # tokenizer.pad_token = tokenizer.eos_token
+    model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
+    # model = LlamaForSequenceClassification.from_pretrained(
+    #     model_name,
+    #     num_labels=5,
+    #     torch_dtype=torch.bfloat16,
+    #     attn_implementation="flash_attention_2",
+    # )
+    # Update the model configuration
+    # model.config.pad_token_id = tokenizer.pad_token_id
+    model.generation_config.pad_token_id = tokenizer.pad_token_id
+    model.generation_config.max_new_tokens = max_length
+
+    train = pd.read_csv('train.csv')
+    train.dropna(inplace=True)  # remove the nans
+
+    new_train = train.copy()
+
+    train['Prompt'] = (
+        "Shorten the following code bug text description while preserving information relevant to its severity/priority. "
+        "Only output the shortened text description and nothing else. Below is the description: \n"
+        + train['Description']
+    )
+                # ('What is the priority (from 0, highest priority, to 4, lowest priority) of the code bug given the '
+                #  'following description. | Component: ') + train['Component'] + " | " + 'Title: ' + train['Title']
+                # + " | " + 'Status: ' + train['Status'] + " | " + 'Resolution: ' + train['Resolution'] + " | " +
+                # 'Description: ' + train['Description'])
+
+    inputs = train['Prompt'].to_numpy()
+
+    for i, x in enumerate(inputs):
+        input_ids = tokenizer(x, return_tensors="pt").to(device)
+
+        outputs = model.generate(**input_ids)
+        new_desc = tokenizer.decode(outputs[0])
+        print(x)
+        print(new_desc)
+        print(new_desc[17+len(x):])
+        exit(0)
+        # print(len(new_desc))
+        # new_desc = new_desc.replace(x, '')
+        # print(f"Shortened: {len(x) - len(new_desc)} | Old len: {len(x)} | New len {len(new_desc)}")
+        # print(new_desc)
+        # print('########################################################################')
+        new_train.iloc[i]['Description'] = new_desc
+
+    new_train.to_csv('shortened_train.csv', index=False)
+
 if __name__ == '__main__':
+    # main()
     main_vllm()
